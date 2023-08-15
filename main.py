b8c2c5fb12e58c1e84b82a0b8d7ed265c455d7a2
Clepalitto <85410090+Clepalitto@users.noreply.github.com>
2023-08-16 01:51:09 +0200

    Update main.py
    Typography

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ how = ["by talking to decision makers", "by mobilizing many fellow citizens", "b
 where = ["in my city", "at my workplace", "in my neighbourhood", "in public space", "at my school/university", "with my friends"]
 activities = ["introduce", "demand", "initiate", "achieve", "recommend", "implement"]
 what = ["fair trade products as standard", "support on public transportation and expansion of cycle paths", "durability as a purchase criterium for new equipment", "sustainable food supply", "electricity from renewable energy", "protection of the oceans"]
-withhom = ["together with my family", "together with my superior", "together with my colleagues", "together with friends", "together with my classmates", "together with my partner"]
+withhom = ["Together with my family", "Together with my superior", "Together with my colleagues", "Together with friends", "Together with my classmates", "Together with my partner"]
 
 phrase = withhom[random.randint(0, 5)]+", I can "+activities[random.randint(0, 5)]+" "+what[random.randint(0, 5)]+" "+where[random.randint(0, 5)]+" "+how[random.randint(0, 5)]
 print(phrase)
